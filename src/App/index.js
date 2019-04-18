9c8c96d1e95849f72934c3f4c9f8d7bf89a5d25f
Ryan Jones <jonesy_222@hotmail.com>
2019-04-18 18:24:51 -0400

    code-changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,98 +1,111 @@
 import React from 'react';
-import Utils from '../utils';
 import './index.css';
 
-export default () => (
-    <div>
-        <main>
-            <div id="landing">
-                <div id="landing-text">
-                    <div id="landing-text-inner">
-                        <h1>Acme Photography</h1>
-                        <h2>Beautiful Natural Photography</h2>
-                        <Utils />
+export default () => {
+    const ref = React.createRef(null);
+    const onButtonClick = () => {
+        ref.current.focus();
+    };
+
+    return (
+        <div>
+            <main>
+                <div id="landing">
+                    <div id="landing-text">
+                        <div id="landing-text-inner">
+                            <h1>Acme Photography</h1>
+                            <h2>Beautiful Natural Photography</h2>
+                            <a
+                                href="#images"
+                                className="btn"
+                                id="view-work"
+                                onClick={onButtonClick}
+                            >
+                                View Work
+                            </a>
+                        </div>
+                    </div>
+                    <div id="landing-image" />
+                </div>
+
+                <div id="images">
+                    <div id="header" ref={ref}>
+                        <h2>Our Work</h2>
+                    </div>
+
+                    <img
+                        src="https://source.unsplash.com/1600x900/?nature,water
+            "
+                        alt=""
+                    />
+                    <div className="caption">
+                        <h3>Photo One</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Ea, facilis?
+                        </p>
+                    </div>
+                    <img
+                        src="https://source.unsplash.com/1600x900/?nature,trees
+            "
+                        alt=""
+                    />
+                    <div className="caption">
+                        <h3>Photo Two</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Ea, facilis?
+                        </p>
+                    </div>
+                    <img
+                        src="https://source.unsplash.com/1600x900/?nature,flowers
+            "
+                        alt=""
+                    />
+                    <div className="caption">
+                        <h3>Photo Three</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Ea, facilis?
+                        </p>
+                    </div>
+                    <img
+                        src="https://source.unsplash.com/1600x900/?nature,animals
+            "
+                        alt=""
+                    />
+                    <div className="caption">
+                        <h3>Photo Four</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Ea, facilis?
+                        </p>
+                    </div>
+                    <img
+                        src="https://source.unsplash.com/1600x900/?nature,clouds
+            "
+                        alt=""
+                    />
+                    <div className="caption">
+                        <h3>Photo Five</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Ea, facilis?
+                        </p>
                     </div>
                 </div>
-                <div id="landing-image" />
-            </div>
+            </main>
 
-            <div id="images">
-                <div id="header">
-                    <h2>Our Work</h2>
-                </div>
-
-                <img
-                    src="https://source.unsplash.com/1600x900/?nature,water
-            "
-                    alt=""
-                />
-                <div className="caption">
-                    <h3>Photo One</h3>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Ea, facilis?
-                    </p>
-                </div>
-                <img
-                    src="https://source.unsplash.com/1600x900/?nature,trees
-            "
-                    alt=""
-                />
-                <div className="caption">
-                    <h3>Photo Two</h3>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Ea, facilis?
-                    </p>
-                </div>
-                <img
-                    src="https://source.unsplash.com/1600x900/?nature,flowers
-            "
-                    alt=""
-                />
-                <div className="caption">
-                    <h3>Photo Three</h3>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Ea, facilis?
-                    </p>
-                </div>
-                <img
-                    src="https://source.unsplash.com/1600x900/?nature,animals
-            "
-                    alt=""
-                />
-                <div className="caption">
-                    <h3>Photo Four</h3>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Ea, facilis?
-                    </p>
-                </div>
-                <img
-                    src="https://source.unsplash.com/1600x900/?nature,clouds
-            "
-                    alt=""
-                />
-                <div className="caption">
-                    <h3>Photo Five</h3>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Ea, facilis?
-                    </p>
-                </div>
-            </div>
-        </main>
-
-        <footer>
-            <h3>Get In Touch</h3>
-            <p>Email or call us to set up a consult.</p>
-            <p>
-                Email: <strong>contact@acmephotos.test</strong>
-            </p>
-            <p>
-                Phone: <strong>(323) 555-5555</strong>
-            </p>
-        </footer>
-    </div>
-);
+            <footer>
+                <h3>Get In Touch</h3>
+                <p>Email or call us to set up a consult.</p>
+                <p>
+                    Email: <strong>contact@acmephotos.test</strong>
+                </p>
+                <p>
+                    Phone: <strong>(323) 555-5555</strong>
+                </p>
+            </footer>
+        </div>
+    );
+};
